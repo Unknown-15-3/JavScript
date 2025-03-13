99f44f214caea8f8947585cfc63ae6a5e05b76d7
Unknown-15-3 <harshinibasubarua@gmail.com>
2025-03-13 20:58:20 +0530

    lesson 5

---
+++ newFile.js
@@ -0,0 +1,16 @@
+<DOCTYPE HTML>
+    <HTML>
+
+        <BODY>
+            <h1 id="myH"></h1>
+            <p id="myP"></p>
+
+            <SCRIPT>
+                document.getElementByID("myH").innerHTML = "JavaScript Comments";
+
+                document.getElementByID("myP").innerHTML = "My First Parahgraph";
+
+            </SCRIPT>
+        </BODY>
+
+    </HTML></>;
